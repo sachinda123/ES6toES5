7dfd028854fea091edc0a2b3377fceb5c8e066ba
sachinda <sachinda@parkwaylabs.com>
2025-01-04 12:59:57 +0530

    first

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// src/index.js
+var greet = function greet(name) {
+  console.log("Hello, ".concat(name, "!"));
+};
+var x = 0;
+var y = 9;
+greet("World");
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+const greet = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+const x = 0;
+let y = 9;
+
+greet("World");
